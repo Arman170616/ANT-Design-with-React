edabc7b551f05b671259b20bfb411e567d68139b
Arman170616 <armanice09@gmail.com>
2023-09-26 11:43:42 +0600

    Loading Button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import {  Button } from 'antd';
 import './App.css';
 import React from 'react';
+// import { useState } from 'react';
 
 function App() {
+
+  const [loading, setLoading] = React.useState(false);
+  const onButtonClick = (e) => {
+    alert('click');
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }
   return (
     <div className="App">
       <header className="App-header">
       
-      <Button type="primary">Button</Button>
+      <Button 
+      type="primary" 
+      block
+      loading={loading}
+      onClick={onButtonClick}
+      >My First Button</Button>
 
       </header>
     </div>
