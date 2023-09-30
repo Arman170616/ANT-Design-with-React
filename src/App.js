ffd5edd877ea7c465bdd92240e3bf47395c88252
Arman170616 <armanice09@gmail.com>
2023-09-30 10:26:19 +0600

    Spinnig with Toggle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import React from 'react';
-import { DatePicker, TimePicker } from 'antd';
+import { Button, Spin } from 'antd';
+
 
 
 function App() {
+  const [loading, setLoading] = React.useState(false);
 
   return (
     <div className="App">
       <header className="App-header">
-        <DatePicker/>
-        <DatePicker.RangePicker/>
-        <TimePicker.RangePicker/>
+        <Spin spinning={loading}></Spin>
+        <Button type="primary" onClick={() => setLoading(!loading)}>Toggle loading</Button>
+        <Spin spinning={loading} tip="Loading...">
+          <div style={{ width: 200, height: 200, backgroundColor: '#ccc' }}></div>
+        </Spin>
 
       </header>
     </div>
