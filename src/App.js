b1a57e3ef0401c9f7159429fa12a4bb91495dff0
Arman170616 <armanice09@gmail.com>
2023-09-30 10:18:52 +0600

    Time & Date picker

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 import './App.css';
 import React from 'react';
-import { Form, Input, Button, message, Alert } from 'antd';
-
-
-
+import { DatePicker, TimePicker } from 'antd';
 
 
 function App() {
-  const [showAlert, setShowAlert] = React.useState(false);
-  const onFinish = (values) => {
-    console.log(values);
-    
-    setTimeout(() => {
-      setShowAlert(true);
-    }, 1000);
-  };
 
   return (
     <div className="App">
       <header className="App-header">
-        {showAlert && 
-        <Alert message="Login Success" 
-        type="success" 
-        showIcon 
-        closable
-        />}
-      <Form onFinish={onFinish}>
-          <Form.Item label="User Name" name="username">
-            <Input placeholder="User name" required></Input>
-          </Form.Item>
-
-          <Form.Item label="Password" name="password">
-            <Input.Password placeholder="Password" required></Input.Password>
-          </Form.Item>
-
-          <Form.Item>
-            <Button block type="primary" htmlType="submit">Log In</Button>
-          </Form.Item>
-        </Form>
-
-
+        <DatePicker/>
+        <DatePicker.RangePicker/>
+        <TimePicker.RangePicker/>
 
       </header>
     </div>
