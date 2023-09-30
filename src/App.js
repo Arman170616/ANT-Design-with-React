3c3fe74fb0f49795f7db261455f7f0e96f8a6f36
Arman170616 <armanice09@gmail.com>
2023-09-30 10:03:06 +0600

    Message Component using message and showAlart

--- src/App.js
+++ src/App.js
@@ -1,79 +1,43 @@
 import './App.css';
 import React from 'react';
-import { Table } from 'antd';
+import { Form, Input, Button, message, Alert } from 'antd';
 
 
 
 
 
 function App() {
-  const data = [
-  {
-    name :'John',
-    age  :32,
-    address : 'New York No. 1 Lake Park',
-    key:'1'
-  },
-  {
-    name :'Kabir',
-    age  :13,
-    address : 'New York No. 1 Lake Park',
-    key:'2'
-  },
-  {
-    name :'Ahmed',
-    age  :50,
-    address : 'New York No. 1 Lake Park',
-    key:'3'
-  },
-  // {
-  //   name :'Kadr',
-  //   age  :100,
-  //   address : 'New York No. 1 Lake Park',
-  //   key:'3'
-  // },
-]
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'key',
-    render: name =>{
-      return <a>{name}</a>
-    }
-
-  },
-
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'key',
-    sorter: (a, b) => a.age - b.age,
-  },
-
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'key',
-  },
-  {
-    title: 'Is Graduated',
-    key: 'key',
-    render: payload =>{
-      return <p>{payload.age>20?'Yes':'No'}</p>
-    }
-  },
-]
+  const [showAlert, setShowAlert] = React.useState(false);
+  const onFinish = (values) => {
+    console.log(values);
+    
+    setTimeout(() => {
+      setShowAlert(true);
+    }, 1000);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <Table
-          dataSource = {data}
-          columns = {columns}
+        {showAlert && 
+        <Alert message="Login Success" 
+        type="success" 
+        showIcon 
+        closable
+        />}
+      <Form onFinish={onFinish}>
+          <Form.Item label="User Name" name="username">
+            <Input placeholder="User name" required></Input>
+          </Form.Item>
 
-        ></Table>
+          <Form.Item label="Password" name="password">
+            <Input.Password placeholder="Password" required></Input.Password>
+          </Form.Item>
+
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">Log In</Button>
+          </Form.Item>
+        </Form>
 
 
 
