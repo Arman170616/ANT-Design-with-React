f2144bbc3b51915d58fce7edc0153a59516a6b54
Arman170616 <armanice09@gmail.com>
2023-09-30 08:54:57 +0600

    Form remind

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import './App.css';
 import React from 'react';
-import {Form, Input, Button} from 'antd';
+import { Form, Input, Button } from 'antd';
+
 
 
 
 function App() {
 
-
-  const onFinish = (e) => {
-    console.log(e);
+  const onFinish = (values) => {
+    console.log('Success:', values);
   }
-
   return (
     <div className="App">
       <header className="App-header">
         <Form onFinish={onFinish}>
           <Form.Item label="User Name" name="username">
-            <Input placeholder="Enter Your Name" required />
+            <Input placeholder="User name" required></Input>
           </Form.Item>
 
           <Form.Item label="Password" name="password">
-            <Input.Password placeholder="Enter Your Password" required />
+            <Input.Password placeholder="Password" required></Input.Password>
           </Form.Item>
 
           <Form.Item>
             <Button block type="primary" htmlType="submit">Log In</Button>
           </Form.Item>
         </Form>
+
+
       </header>
     </div>
   );
