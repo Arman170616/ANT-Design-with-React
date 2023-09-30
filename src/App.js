21efd25cffb5ecef1270f369664165cc04113a70
Arman170616 <armanice09@gmail.com>
2023-09-30 09:25:34 +0600

    Table commponets:

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
 import './App.css';
 import React from 'react';
-import { Form, Input, Button } from 'antd';
+import { Table } from 'antd';
+
 
 
 
 
 function App() {
+  const data = [
+  {
+    name :'John',
+    age  :32,
+    address : 'New York No. 1 Lake Park',
+    key:'1'
+  },
+  {
+    name :'Kabir',
+    age  :13,
+    address : 'New York No. 1 Lake Park',
+    key:'2'
+  },
+  {
+    name :'Ahmed',
+    age  :50,
+    address : 'New York No. 1 Lake Park',
+    key:'3'
+  },
+  // {
+  //   name :'Kadr',
+  //   age  :100,
+  //   address : 'New York No. 1 Lake Park',
+  //   key:'3'
+  // },
+]
 
-  const onFinish = (values) => {
-    console.log('Success:', values);
-  }
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'key',
+    render: name =>{
+      return <a>{name}</a>
+    }
+
+  },
+
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'key',
+    sorter: (a, b) => a.age - b.age,
+  },
+
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'key',
+  },
+  {
+    title: 'Is Graduated',
+    key: 'key',
+    render: payload =>{
+      return <p>{payload.age>20?'Yes':'No'}</p>
+    }
+  },
+]
+
   return (
     <div className="App">
       <header className="App-header">
-        <Form onFinish={onFinish}>
-          <Form.Item label="User Name" name="username">
-            <Input placeholder="User name" required></Input>
-          </Form.Item>
+        <Table
+          dataSource = {data}
+          columns = {columns}
 
-          <Form.Item label="Password" name="password">
-            <Input.Password placeholder="Password" required></Input.Password>
-          </Form.Item>
+        ></Table>
 
-          <Form.Item>
-            <Button block type="primary" htmlType="submit">Log In</Button>
-          </Form.Item>
-        </Form>
 
 
       </header>
